d92a5062a970fecb24fc59cae860ae3743d4c9b9
Camila Kunitz <camila.kunitz@gmail.com>
2024-01-29 15:41:12 -0300

    Projeto criado

---
+++ src/test/java/com/camilakunitz/projetomc/ProjetomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camilakunitz.projetomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
