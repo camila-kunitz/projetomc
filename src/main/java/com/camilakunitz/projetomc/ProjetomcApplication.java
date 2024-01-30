62f4040d9e91a0b9dee46b1e413ca889c817b3f5
Camila Kunitz <camila.kunitz@gmail.com>
2024-01-30 10:59:55 -0300

    Criando operacao de instanciacao

--- src/main/java/com/camilakunitz/projetomc/ProjetomcApplication.java
+++ src/main/java/com/camilakunitz/projetomc/ProjetomcApplication.java
@@ -1,13 +1,32 @@
 package com.camilakunitz.projetomc;
 
+import com.camilakunitz.projetomc.domain.Categoria;
+import com.camilakunitz.projetomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjetomcApplication {
+public class ProjetomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
